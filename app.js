7a4b6ed2e3491759c22cf46d010ee42daac05ac1
João Paulo <joaoantunees@gmail.com>
2023-04-04 16:44:02 -0300

    refactor: refeito EJS

--- app.js
+++ app.js
@@ -9,38 +9,23 @@ const port  = 5000
 
 app.set('view engine', 'ejs')     // Setamos que nossa engine será o ejs
 app.use(expressLayouts)           // Definimos que vamos utilizar o express-ejs-layouts na nossa aplicação
+//config. Body-Parser
 app.use(bodyParser.urlencoded())  // Com essa configuração, vamos conseguir parsear o corpo das requisições
 
 app.use(express.static(__dirname + '/public'))
+
+//connect port
 app.listen(port, () => {
-    console.log(`A mágica acontece em http://localhost:${port}`)
+    console.log(`http://localhost:${port}`)
 })
 
 //routes
-
 app.get('/', (req, res) => {
   return res.render('pages/home')
 })
 
-app.get('/about', (req, res) => {
-  var users = [{
-      name: faker.name.findName(),
-      email: faker.internet.email(),
-      avatar: 'http://placebear.com/300/300'
-  }, {
-      name: faker.name.findName(),
-      email: faker.internet.email(),
-      avatar: 'http://placebear.com/400/300'
-  }, {
-      name: faker.name.findName(),
-      email: faker.internet.email(),
-      avatar: 'http://placebear.com/500/300'
-  }]
-
-  res.render('pages/about', {
-      usuarios: users
-  })
-})
+app.post('/about', (req, res) => {
+  res.send('Texto: ' + req.body.titulo + "Conteúdo: " + req.body.conteudo)})
 
 app.get('/contact', (req, res) => {
   res.render('pages/contact')
